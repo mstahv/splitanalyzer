6cdc666114f89d6c0884662b208c2062431bd4c3
Matti Tahvonen <matti@vaadin.com>
2014-04-16 20:50:45 +0300

    Trying (blindly) to make parser accept split times without "recalculated" positions
    Also some refactoring

--- src/main/java/org/peimari/splits/client/AaltonenHtmlParser.java
+++ src/main/java/org/peimari/splits/client/AaltonenHtmlParser.java
@@ -1,6 +1,5 @@
 package org.peimari.splits.client;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NodeList;
@@ -39,57 +38,31 @@ public class AaltonenHtmlParser {
 
                             Result result = new Result();
                             Person person = new Person();
-                            final String innerText = namecell.getFirstChildElement().getInnerText();
-                            person.setFirstName(innerText.substring(0, innerText.indexOf(" ")));
-                            person.setLastName(innerText.substring(innerText.indexOf(" ")+1));
+                            final String innerText = namecell.
+                                    getFirstChildElement().getInnerText();
+                            person.setFirstName(innerText.substring(0,
+                                    innerText.indexOf(" ")));
+                            person.setLastName(innerText.substring(innerText.
+                                    indexOf(" ") + 1));
                             result.setPerson(person);
 
                             String rcell = tds.getItem(tds.getLength() - 2).
                                     getInnerText();
+                            // only try to parse splits if result is available
                             if (rcell.matches("[0-9].*")) {
                                 result.setCompetitorStatus(
                                         CompetitorStatus.OK);
-                                for (int k = 2; k < tds.getLength() - 2; k++) {
-                                    Element el = tds.getItem(k);
-                                    String timestr = el.getElementsByTagName(
-                                            "span").
-                                            getItem(0).getInnerText();
-                                    if (timestr.equals("-")) {
-                                        // missed punch
-                                        SplitTime splitTime = new SplitTime();
-                                        result.getSplitTimes().add(splitTime);
-                                    } else {
-                                        timestr = timestr.substring(0, timestr.
-                                                indexOf("(") - 1);
-                                        String[] split2 = timestr.split(":");
-                                        int curCumu = 0;
-                                        for (int l = split2.length - 1; l >= 0; l--) {
-                                            String s = split2[l].trim();
-                                            int m = split2.length - l - 1;
-                                            curCumu += (int) (Integer.
-                                                    parseInt(s) * Math.
-                                                    pow(60, m) * 1000);
-                                        }
-                                        SplitTime splitTime = new SplitTime();
-                                        splitTime.setTime(curCumu);
-                                        result.getSplitTimes().add(splitTime);
-                                    }
+                                parseSplits(tds, result);
+                                long time = result.getSplitTimes()
+                                        .get(result.getSplitTimes().size() - 1)
+                                        .getTime();
+                                result.setTime(time);
+                                if (time != 0) {
+                                    classResult.getResults().add(result);
                                 }
-                            } else {
-                                continue;
-                            }
-
-                            long time = result.getSplitTimes()
-                                    .get(result.getSplitTimes().size() - 1)
-                                    .getTime();
-                            result.setTime(time);
-                            if(time != 0) {
-                                classResult.getResults().add(result);
                             }
                         }
                     } catch (Exception e) {
-                        GWT.debugger();
-                        e.printStackTrace();
                     }
                 }
 
@@ -97,12 +70,45 @@ public class AaltonenHtmlParser {
         }
 
         if (classResult.getResults().isEmpty()) {
-            throw new RuntimeException("Not an 'aaltonen format' ");
+            throw new RuntimeException("Not an 'aaltonen format' !? ");
         }
 
         rl.getClassResults().add(classResult);
 
         return rl;
+    }
+
+    private void parseSplits(NodeList<Element> tds, Result result) throws NumberFormatException {
+        for (int k = 2; k < tds.getLength() - 2; k++) {
+            Element el = tds.getItem(k);
+            String timestr = el.getElementsByTagName(
+                    "span").
+                    getItem(0).getInnerText();
+            if (timestr.equals("-")) {
+                // missed punch
+                SplitTime splitTime = new SplitTime();
+                result.getSplitTimes().add(splitTime);
+            } else {
+                // positions not always calculated by server, scheduled ?
+                if (timestr.contains("(")) {
+                    timestr = timestr.substring(0,
+                            timestr.
+                            indexOf("(") - 1);
+                }
+                String[] split2 = timestr.split(":");
+                int curCumu = 0;
+                for (int l = split2.length - 1; l >= 0; l--) {
+                    String s = split2[l].trim();
+                    int m = split2.length - l - 1;
+                    curCumu += (int) (Integer.
+                            parseInt(s) * Math.
+                            pow(60, m) * 1000);
+                }
+                SplitTime splitTime = new SplitTime();
+                splitTime.setTime(curCumu);
+                result.getSplitTimes().add(splitTime);
+            }
+        }
     }
 
 }
